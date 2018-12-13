5b5bddefd92d0cd6610d3100d6179cfb44e5c237
Rebecca Le <traybaby@gmail.com>
2018-12-14 04:05:51 +0800

    Add `aoc` function to change me into my Advent of Code folder for the given year

---
+++ fish/completions/aoc.fish
@@ -0,0 +1,1 @@
+complete -c aoc -a "(cd ~/Projects/personal/elixir/advent_of_code/; and find . -maxdepth 3 -type d)"
---
+++ fish/functions/aoc.fish
@@ -0,0 +1,3 @@
+function aoc
+  cd ~/Projects/personal/elixir/advent_of_code/$argv[1]
+end
