3f7d3c0ea86b545fb6e9a3112e6f4d75dea826f6
Rebecca Le <traybaby@gmail.com>
2019-03-05 16:07:17 +0800

    Add inflate script while reading Building Git

---
+++ fish/functions/inflate.fish
@@ -0,0 +1,5 @@
+# Defined in - @ line 0
+# Written while reading Building Git, section 2.3.2 - Blobs on Disk
+function inflate --description 'alias inflate=elixir ~/Projects/personal/elixir/exit/lib/utils/inflate.exs'
+	elixir ~/Projects/personal/elixir/exit/lib/utils/inflate.exs $argv;
+end
