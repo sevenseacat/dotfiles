3e338fc0c9c8e14510ba4426661ed2f0378d8651
Rebecca Le <traybaby@gmail.com>
2020-04-20 12:55:58 +0800

    Add config options to get Erlang to install correctly under OSX Catalina
    https://github.com/asdf-vm/asdf-erlang/issues/82

--- fish/config.fish
+++ fish/config.fish
@@ -3,3 +3,6 @@ source /usr/local/opt/asdf/asdf.fish
 set -U BUNDLER_EDITOR sublime
 set -U EDITOR nano
 set -U HOMEBREW_NO_ANALYTICS 1
+
+set -U KERL_CONFIGURE_OPTIONS "--disable-hipe --with-ssl=(brew --prefix openssl)"
+set -U CFLAGS "-O2 -g -fno-stack-check"
