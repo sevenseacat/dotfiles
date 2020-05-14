2a8cebeadca52ce8e3dd434e3cdda91093b418a1
Rebecca Le <rebecca@alembic.com.au>
2020-05-14 13:44:34 +0800

    Fix GPG signing issue

--- fish/config.fish
+++ fish/config.fish
@@ -4,5 +4,8 @@ set -U BUNDLER_EDITOR sublime
 set -U EDITOR nano
 set -U HOMEBREW_NO_ANALYTICS 1
 
-set -U KERL_CONFIGURE_OPTIONS "--disable-hipe --with-ssl=(brew --prefix openssl)"
+set -U KERL_CONFIGURE_OPTIONS "--without-javac --disable-hipe --with-ssl=(brew --prefix openssl)"
 set -U CFLAGS "-O2 -g -fno-stack-check"
+
+# Needed for commit signing in Git
+set -x GPG_TTY (tty)
